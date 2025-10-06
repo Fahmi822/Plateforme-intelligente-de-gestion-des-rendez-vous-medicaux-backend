e2cc5008c4f3b1e3940ac3e31f06cc5d3711dcd1
fahmihlel <fahmihlel822@gmail.com>
2025-10-06 11:42:11 +0200

    Initial commit - projet Spring Boot docTime

---
+++ src/main/java/com/example/gestiondesrendezvousmedicauxbackend/PlateformeIntelligenteDeGestionDesRendezVousMedicauxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestiondesrendezvousmedicauxbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlateformeIntelligenteDeGestionDesRendezVousMedicauxBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlateformeIntelligenteDeGestionDesRendezVousMedicauxBackendApplication.class, args);
+    }
+
+}
